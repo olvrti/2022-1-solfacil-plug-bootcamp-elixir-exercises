df805476b123bd18b5a6c358774bee99b41d9518
Tiago de Oliveira <deoliv.tiago@outlook.com>
2022-03-15 23:56:31 -0300

    bird count

--- bird-count/lib/bird_count.ex
+++ bird-count/lib/bird_count.ex
@@ -1,21 +1,29 @@
 defmodule BirdCount do
-  def today(list) do
-    # Please implement the today/1 function
-  end
+  def today([h | _t] = list) when is_list(list), do: h
 
-  def increment_day_count(list) do
-    # Please implement the increment_day_count/1 function
-  end
+  def today(_list), do: nil
 
-  def has_day_without_birds?(list) do
-    # Please implement the has_day_without_birds?/1 function
-  end
+  def increment_day_count([]), do: [1]
 
-  def total(list) do
-    # Please implement the total/1 function
-  end
+  def increment_day_count([h | t]), do: [h + 1] ++ t
 
-  def busy_days(list) do
-    # Please implement the busy_days/1 function
-  end
+  def has_day_without_birds?([]), do: false
+
+  def has_day_without_birds?([0 | _t]), do: true
+
+  def has_day_without_birds?([_h | t]), do: has_day_without_birds?(t)
+
+  def total([]), do: 0
+
+  def total([h]), do: h
+
+  def total([h | [b | t]]), do: total([h + b] ++ t)
+
+  def busy_days(list), do: busy_days(list, 0)
+
+  def busy_days([], acc), do: acc
+
+  def busy_days([h | t], acc) when h < 5, do: busy_days(t, acc)
+
+  def busy_days([_h | t], acc), do: busy_days(t, acc + 1)
 end
